41c5fbe4f2a38c9097580ada17b60e040366d353
App Academy Student <student@appacademy.io>
2016-04-11 10:23:39 -0700

    myBoundgit add .git add .git add .git add .git add .git add .git add .

--- arguments_exercises.js
+++ arguments_exercises.js
@@ -1,4 +1,5 @@
 function sum() {
+  console.log(this);
   var args = Array.prototype.slice.call(arguments);
   return args.reduce(function(accum, el) {
     return accum + el;
@@ -6,3 +7,17 @@ function sum() {
 }
 
 console.log(sum(1,2,3,4));
+
+Function.prototype.myBind = function(context) {
+  var args = Array.prototype.slice.call(arguments).slice(1);
+  var fn = this;
+  return function(){
+    return fn.apply(context, args);
+  };
+};
+
+var Dog = {
+  name: "Rufus"
+};
+
+console.log(sum.myBind(Dog, 1, 2, 3)());
