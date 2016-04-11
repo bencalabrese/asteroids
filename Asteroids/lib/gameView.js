dd88907db02b5d5670cc62e26102c925a28b687f
App Academy Student <student@appacademy.io>
2016-04-11 15:19:20 -0700

    implemented GameView

---
+++ Asteroids/lib/gameView.js
@@ -0,0 +1,14 @@
+var Game = require("./game.js");
+
+function GameView(game, context) {
+  this.game = game;
+  this.context = context;
+}
+
+GameView.prototype.start = function() {
+  var self = this;
+  setInterval(function() {
+    self.game.moveObjects();
+    self.game.draw(self.context);
+  }, 20);
+};
