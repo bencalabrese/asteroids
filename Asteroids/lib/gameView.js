eb8b7186d78ba0741c6979d0563e619f301fcb7b
App Academy Student <student@appacademy.io>
2016-04-11 18:06:26 -0700

    started to implement keybindings

--- Asteroids/lib/gameView.js
+++ Asteroids/lib/gameView.js
@@ -1,4 +1,5 @@
 var Game = require("./game.js");
+var key = require("../keymaster.js");
 
 function GameView(game, context) {
   this.game = game;
@@ -7,9 +8,16 @@ function GameView(game, context) {
 
 GameView.prototype.start = function() {
   var self = this;
+  this.bindKeyHandlers();
   setInterval(function() {
     self.game.step(self.context);
   }, 20);
+};
+
+GameView.prototype.bindKeyHandlers = function () {
+  key('enter', function(){
+    alert('you pressed a!');
+  });
 };
 
 module.exports = GameView;
